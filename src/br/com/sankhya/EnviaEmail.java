56a10cd9492bad035cc47c6afb9eaab489de11c4
gadrianosl <gadrianosl@gmail.com>
2017-11-09 14:22:03 -0200

    nnn

--- src/br/com/sankhya/EnviaEmail.java
+++ src/br/com/sankhya/EnviaEmail.java
@@ -1,3 +1,9 @@
+/* ------------------------------------------------
+ * AUTOR:           ADRIANO
+ * DATA DE CRIAÇAO: 16/10/2017
+ * DESCRIÇÃO:       ENVIA EMAIL USANDO JAVA.MAIL
+ --------------------------------------------------*/
+
 package br.com.sankhya;
 
 import java.util.Calendar;
@@ -13,23 +19,23 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
-public class EnviaEmail //implements AcaoRotinaJava 
+import org.cuckoo.core.ScheduledAction;
+import org.cuckoo.core.ScheduledActionContext;
+
+public class EnviaEmail implements ScheduledAction
 {
-     public static void main(String[] args)
-      
-	//@Override
-	//public void doAction(ContextoAcao contexto) throws Exception 
+	@Override
+	public void onTime(ScheduledActionContext arg0) 
       
       {
             Properties props = new Properties();
-            /** Parâmetros de conexão com servidor  */
+            
+            // Parâmetros de conexão com servidor  
             props.put("mail.smtp.host", "email-ssl.com.br");
             props.put("mail.smtp.socketFactory.port", "465");
             props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
             props.put("mail.smtp.auth", "true");
-            props.put("mail.smtp.port", "465");
-
-            //teste 
+            props.put("mail.smtp.port", "465"); 
             
             Session session = Session.getDefaultInstance(props,
                         new javax.mail.Authenticator() {
@@ -38,14 +44,17 @@ public class EnviaEmail //implements AcaoRotinaJava
                                    return new PasswordAuthentication("adriano.soares@medika.com.br", "adrianomedika");
                              }
                         });
-            /** Ativa Debug para sessão */
+            
+            // Ativa Debug para sessão 
             session.setDebug(true);
             try {
 
                   Message message = new MimeMessage(session);
-                  message.setFrom(new InternetAddress("adriano.soares@medika.com.br")); //Remetente
+                  //Remetente
+                  message.setFrom(new InternetAddress("adriano.soares@medika.com.br")); 
 
-                  Address[] toUser = InternetAddress //Destinatário(s)
+                  //Destinatário(s)
+                  Address[] toUser = InternetAddress 
                              .parse("gadrianosl@gmail.com, adriano.soares@medika.com.br");  
                   message.setRecipients(Message.RecipientType.TO, toUser);
                   
@@ -57,18 +66,17 @@ public class EnviaEmail //implements AcaoRotinaJava
                   message.setSentDate(gc.getTime());
                   message.setSubject("Enviando email com JavaMail - Re: 122892");//Assunto
                   message.setText("Enviei este email utilizando JavaMail com minha conta GMail!");
-                  /**Método para enviar a mensagem criada*/
+                  
+                  //Método para enviar a mensagem criada
                   Transport.send(message);
                   
                   StringBuffer mensagem = new StringBuffer();
       		      mensagem.append("Email enviado!");
-
-      			  //contexto.setMensagemRetorno(mensagem.toString());
                   
                   System.out.println(mensagem);
                   
              } catch (MessagingException e) {
                   throw new RuntimeException(e);
             }
-      }
+      } 
 }
